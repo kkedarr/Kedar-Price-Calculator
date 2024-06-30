a516d18b87dcf8129c951aae7c251e46caba485f
EliteBook <omodaraomokayode@gmail.com>
2024-06-30 15:44:26 +0100

    Updates

--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import watchesReducer from "./features/watches/watchesSlice";
-
-export const store = configureStore({
-  reducer: {
-    watches: watchesReducer,
-  },
-});
